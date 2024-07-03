6627aa962a383dc3ae9353bbb4d4d0a226fec794
SUMMERxKx <samar.k.khajuria@gmail.com>
2024-07-03 12:25:50 -0700

    Made The boiler plates and downloaded the assets

---
+++ src/components/Experience.jsx
@@ -0,0 +1,9 @@
+const Experience = () => {
+    return (
+      <div id = "experience" className="section">
+        <h1>Experience</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti dolores quidem voluptatem rem voluptate maxime debitis distinctio laboriosam labore. Veritatis suscipit doloremque dicta aliquid exercitationem dolore corrupti consequuntur tenetur quibusdam.</p>
+      </div>
+    );
+  };
+  export default Experience;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return (
+      <div id = "hero" className="section">
+        <h1>Hero section</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti dolores quidem voluptatem rem voluptate maxime debitis distinctio laboriosam labore. Veritatis suscipit doloremque dicta aliquid exercitationem dolore corrupti consequuntur tenetur quibusdam.</p>
+      </div>
+    );
+  };
+  export default Hero;
